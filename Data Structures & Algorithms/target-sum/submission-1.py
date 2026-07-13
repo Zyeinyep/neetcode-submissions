5f543533e07b097343fd13df769cfe69827ca11a
Zyeinyep Khaval <89563758+Zyeinyep@users.noreply.github.com>
2026-07-12 20:08:23 -0400

    Add: target-sum - submission-1

---
+++ Data Structures & Algorithms/target-sum/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        count = 0
+        signs = [1,-1]
+        def backtrack(start, total):
+            nonlocal count
+            if start == len(nums):
+                if total == target:
+                    count = count + 1
+                return
+            
+            backtrack(start+1, total+(nums[start]))
+            backtrack(start+1, total-(nums[start]))
+        backtrack(0,0)
+        return count
